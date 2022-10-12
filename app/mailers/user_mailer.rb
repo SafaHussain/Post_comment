655b53c2d6f315ca7cbf634de4f2014b157ea1d5
SafaHussain <sanasweetu4@gmail.com>
2022-10-12 10:20:52 +0500

    Sending mail when user is created

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,11 @@
 class UserMailer < ApplicationMailer
    
-   def post_status
+   def user_status 
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Status of your account')
+   end
+   
+    def post_status
     @user = params[:user]
     mail(to: @user.email, subject: 'Status of your recent post')
     end  
